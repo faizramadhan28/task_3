eee649c0853fd7d6239ce3dde97f39a47e0e2a6d
faizramadhan28 <ramadhanfaiz2801@gmail.com>
2022-11-03 19:36:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(e) {
+    if (e.target.className ==  'thumb') {
+
+
+        jumbo.src = e.target.src;
+
+    }
+});
